996694ba880e38fc23518d6e786804f98f253c5c
KlopKlopovnikoff <Honegoren@mail.ru>
2026-05-04 01:59:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://klopklopovnikoff.github.io/DZ_React/"
+  base: "/DZ_React/"
 })
